5d22adad56684a7fd272b48fdfe90089c5410c67
Matheus Juan <47642703+MatheusJuan@users.noreply.github.com>
2023-07-04 00:02:32 -0300

    scroll teste 08

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var scrollbar = document.querySelector("#my-scrollbar");
-    var scrollbarInstance = SmoothScrollbar.init(scrollbar);
+    var options = {}; // Opções de configuração, se necessário
+    var scrollbar = SmoothScrollbar.init(document.querySelector("#my-scrollbar"), options);
   });
